b3aa8da6b155813ac6ee4395e8ca5926004ebdea
nbrindza <nbrindza@enterprisespectrum.com>
2015-01-06 12:21:21 -0800

    Update to allow for Volume table with no target rate adjustment

--- Best_View_Dbase/bvt_prod/Views/UCLM_Flightplan_Volume_Forecast_VW.sql
+++ Best_View_Dbase/bvt_prod/Views/UCLM_Flightplan_Volume_Forecast_VW.sql
@@ -1,13 +1,14 @@
 ﻿
 
-
-
+drop view [bvt_prod].[UCLM_Flightplan_Volume_Forecast_VW]
+go
 
 CREATE view [bvt_prod].[UCLM_Flightplan_Volume_Forecast_VW]
 as 
 select idFlight_Plan_Records
 	--------------Logic to determine where to get volume
-	, Case when idVolume_Type_LU_TBL_FK=1 then Lead_Volumes.Volume* Target_adjustment_start_end.Volume_Adjustment
+	, Case when idVolume_Type_LU_TBL_FK=1 and Target_adjustment_start_end.Volume_Adjustment is not null then Lead_Volumes.Volume* Target_adjustment_start_end.Volume_Adjustment
+		when idVolume_Type_LU_TBL_FK=1 then Lead_Volumes.Volume
 		when idVolume_Type_LU_TBL_FK=2 then	Flight_Plan_Records_Volume.Volume 
 		when idVolume_Type_LU_TBL_FK=3 then sum(Flight_Plan_Record_Budgets.Budget)/sum(CPP_Start_End.CPP)
 		end as Volume
