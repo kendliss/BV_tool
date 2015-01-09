1edcb43ecc04e2ff7bf9b8e2ea5c879be3eaefe1
nbrindza <nbrindza@enterprisespectrum.com>
2015-01-08 23:34:41 -0800

    More removal of non bv tool portions

--- Best_View_Dbase/staging/Tables/'Curve Data_'.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [staging].['Curve Data$'] (
-    [ID]      NVARCHAR (255) NULL,
-    [WEEK]    FLOAT (53)     NULL,
-    [Call_P]  FLOAT (53)     NULL,
-    [Click_P] FLOAT (53)     NULL
-);
-
--- Best_View_Dbase/staging/Tables/UVAQ_CPP_translation.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [staging].[UVAQ_CPP_translation] (
-    [Touch_Type_id] INT        NULL,
-    [CPP_Category]  INT        NULL,
-    [CPP]           FLOAT (53) NULL,
-    [Billtiming]    INT        NULL,
-    [start_date]    DATETIME   NULL
-);
-
