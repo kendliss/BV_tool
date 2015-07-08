1cb3cbef8f7bf6b36970e4af1a240aacf5aaa1c8
Nathan Brindza <nbrindza@enterprisespectrum.com>
2015-07-08 07:58:58 -0700

    Fixed to assign media month to actuals query.

---
+++ Best_View_Dbase/bvt_prod/Tables/FPR_Manual_CPP.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [bvt_prod].[FPR_Manual_CPP]
+(
+	[IdFPR_Manual_CPP] INT IDENTITY (1, 1) NOT NULL PRIMARY KEY, 
+    [idCPP_Category_FK] INT NOT NULL, 
+    [Bill_Month] INT NOT NULL, 
+    [Bill_Year] INT NOT NULL, 
+    [CPP] FLOAT NULL, 
+    [idFlight_Plan_Record_FK] INT NOT NULL, 
+    CONSTRAINT [FK_FPR_Manual_CPP_CPP_Category] FOREIGN KEY ([idCPP_Category_FK]) REFERENCES [bvt_prod].[CPP_Category]([idCPP_Category]), 
+    CONSTRAINT [FK_FPR_Manual_CPP_Flight_Plan_Records] FOREIGN KEY ([idFlight_Plan_Record_FK]) REFERENCES [bvt_prod].[Flight_Plan_Records]([idFlight_Plan_Records])
+)
