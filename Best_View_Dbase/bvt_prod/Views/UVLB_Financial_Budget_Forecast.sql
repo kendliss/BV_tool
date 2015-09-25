43206b8e1bd4bfee4190ac9dc397161def52d3f1
nbrindza <nbrindza@enterprisespectrum.com>
2015-09-25 16:26:33 -0700

    Updated UVLB Financial Budget view to use the drop date instead of inhome, need to document that hairy logic in the case statement soon.

--- Best_View_Dbase/bvt_prod/Views/UVLB_Financial_Budget_Forecast.sql
+++ Best_View_Dbase/bvt_prod/Views/UVLB_Financial_Budget_Forecast.sql
@@ -1,4 +1,6 @@
-﻿
+﻿drop view [bvt_prod].[UVLB_Financial_Budget_Forecast]
+GO
+
 create view [bvt_prod].[UVLB_Financial_Budget_Forecast]
 as select 
 	flight_plan_records.idFlight_Plan_Records
@@ -17,18 +19,23 @@ as select
 	, Offer
 	
 	, case when Budget_Type_LU_TBL_idBudget_Type_LU_TBL=2 then Bill_Month
+		when [idCPP_Category_FK]=5 then month(DATEADD(month,bill_timing,dateadd(day,[Days_Before_Inhome],Flight_Plan_Records.InHome_Date))) 
 		when MONTH(Flight_Plan_Records.InHome_Date)=12 then 12
-		else month(DATEADD(month,bill_timing,Flight_Plan_Records.InHome_Date)) 
+		else month(DATEADD(month,bill_timing,dateadd(day,[Days_Before_Inhome],Flight_Plan_Records.InHome_Date))) 
 		end as bill_month
 	, case when Budget_Type_LU_TBL_idBudget_Type_LU_TBL=2 then Bill_Year
-		when month(Flight_Plan_Records.InHome_Date)=12 then YEAR(Flight_Plan_Records.inhome_date)
-		else year(DATEADD(month,bill_timing,Flight_Plan_Records.InHome_Date)) 
+		when [idCPP_Category_FK]=5 then year(DATEADD(month,bill_timing,dateadd(day,[Days_Before_Inhome],Flight_Plan_Records.InHome_Date))) 
+		when MONTH(Flight_Plan_Records.InHome_Date)=12 then year(Flight_Plan_Records.InHome_Date)
+		else year(DATEADD(month,bill_timing,dateadd(day,[Days_Before_Inhome],Flight_Plan_Records.InHome_Date))) 
 		end as bill_year
 	, case when Budget_Type_LU_TBL_idBudget_Type_LU_TBL=2 then Budget
 		else CPP*Volume end as budget
 	, mediaweek
 	
 	from bvt_processed.UVLB_Flight_Plan as flight_plan_records
+		left join [bvt_processed].[Dropdate_Start_End]
+			on flight_plan_records.[idProgram_Touch_Definitions_TBL_FK]=[Dropdate_Start_End].[idProgram_Touch_Definitions_TBL_FK]
+			and flight_plan_records.InHome_Date between [Dropdate_Start_End].[drop_start_date] and [Dropdate_Start_End].[END_DATE]
 		left join bvt_prod.Flight_Plan_Record_Budgets
 			on flight_plan_records.idFlight_Plan_Records=idFlight_Plan_Records_FK
 		LEFT join bvt_processed.CPP_Start_End
@@ -45,17 +52,7 @@ as select
 		when month(Flight_Plan_Records.InHome_Date)=12 then YEAR(Flight_Plan_Records.inhome_date)
 		else year(DATEADD(month,bill_timing,Flight_Plan_Records.InHome_Date)) 
 		end)=A.MediaMonth_Year
-		left join
 		-----Bring in touch definition labels 
-(select idProgram_Touch_Definitions_TBL, Touch_Name, Program_Name, Tactic, Media, Audience, Creative_Name, Goal, Offer, Campaign_Type
-		 from bvt_prod.Program_Touch_Definitions_TBL
-			left join bvt_prod.Audience_LU_TBL on idAudience_LU_TBL_FK=idAudience_LU_TBL
-			left join bvt_prod.Campaign_Type_LU_TBL on idCampaign_Type_LU_TBL_FK=idCampaign_Type_LU_TBL
-			left join bvt_prod.Creative_LU_TBL on idCreative_LU_TBL_fk=idCreative_LU_TBL
-			left join bvt_prod.Goal_LU_TBL on idGoal_LU_TBL_fk=idGoal_LU_TBL
-			left join bvt_prod.Media_LU_TBL on idMedia_LU_TBL_fk=idMedia_LU_TBL
-			left join bvt_prod.Offer_LU_TBL on idOffer_LU_TBL_fk=idOffer_LU_TBL
-			left join bvt_prod.Program_LU_TBL on idProgram_LU_TBL_fk=idProgram_LU_TBL
-			left join bvt_prod.Tactic_LU_TBL on idTactic_LU_TBL_fk=idTactic_LU_TBL) as touchdef
-		on Flight_Plan_Records.idProgram_Touch_Definitions_TBL_FK=idProgram_Touch_Definitions_TBL 
+		left join [bvt_prod].[Touch_Definition_VW]
+			on Flight_Plan_Records.idProgram_Touch_Definitions_TBL_FK=[Touch_Definition_VW].idProgram_Touch_Definitions_TBL 
 
