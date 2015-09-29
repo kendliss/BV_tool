42f9701289a95571977ab7b9de4f723ac2b668c0
kendliss <kliss@enterprisespectrum.com>
2015-09-29 15:32:58 -0500

    added UCLM_GP Program
    All the SPs, Views, and tables needed to create a UCLM_GP Best View
    Pivot

---
+++ Best_View_Dbase/bvt_processed/Stored Procedures/UCLM_GP_Best_View_Forecast_WKLY_PR.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [bvt_prod].[UCLM_GP_Best_View_Forecast_WKLY_PR]
+as
+declare @lst_load datetime
+select @lst_load = (select MAX(load_dt) from bvt_processed.UCLM_GP_Best_View_Forecast)
+select [idFlight_Plan_Records], [Campaign_Name], [InHome_Date], [Media_Year], [Media_Week], [Media_Month], 
+[Touch_Name], [Program_Name], [Tactic], [Media], [Campaign_Type], [Audience], [Creative_Name], 
+[Goal], [Offer], [KPI_Type], [Product_Code], sum([Forecast]) as Forecast, load_dt
+
+from bvt_processed.UCLM_GP_Best_View_Forecast
+where load_dt= @lst_load
+group by [idFlight_Plan_Records], [Campaign_Name], [InHome_Date], [Media_Year], [Media_Week], [Media_Month], 
+[Touch_Name], [Program_Name], [Tactic], [Media], [Campaign_Type], [Audience], [Creative_Name], 
+[Goal], [Offer], [KPI_Type], [Product_Code], load_dt
+RETURN 0
---
+++ Best_View_Dbase/bvt_prod/Views/UCLM_GP_FlightplanSalesForecast.sql
@@ -0,0 +1,14 @@
+﻿
+
+create view [bvt_prod].[UCLM_GP_FlightplanSalesForecast]
+as 
+select 
+	UCLM_GP_Flightplan_SalesRate_Daily_VW.idFlight_Plan_Records
+	, idkpi_type_FK as idkpi_types_FK
+	, idProduct_LU_TBL_FK
+	, Forecast_DayDate
+	, Sales_Rate_Daily*Volume as Sales_Forecast
+from bvt_prod.UCLM_GP_Flightplan_SalesRate_Daily_VW
+	inner join bvt_prod.UCLM_GP_Flightplan_Volume_Forecast_VW
+		on UCLM_GP_Flightplan_SalesRate_Daily_VW.idFlight_Plan_Records=UCLM_GP_Flightplan_Volume_Forecast_VW.idFlight_Plan_Records
+
