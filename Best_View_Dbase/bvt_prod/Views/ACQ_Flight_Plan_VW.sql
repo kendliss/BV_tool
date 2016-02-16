23261fef06a62369f894e275fc942236297ca8ce
kendliss <kliss@enterprisespectrum.com>
2016-02-16 12:57:44 -0600

    added ACQ
    Acquisition views

---
+++ Best_View_Dbase/bvt_prod/Views/ACQ_Flight_Plan_VW.sql
@@ -0,0 +1,7 @@
+﻿drop view bvt_prod.ACQ_Flight_Plan_VW
+go
+
+create view bvt_prod.ACQ_Flight_Plan_VW
+as select * from bvt_prod.Flight_Plan_Records 
+where idProgram_Touch_Definitions_TBL_FK in (SELECT * FROM bvt_prod.Program_Selector('ACQ'))
+go
