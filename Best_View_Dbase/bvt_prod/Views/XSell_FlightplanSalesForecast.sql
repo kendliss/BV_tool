2602c4f5d326cd6eb161d3f0c71d2ea3e99d20b4
kendliss <kliss@enterprisespectrum.com>
2016-04-05 13:15:48 -0500

    Created xSell
    Created XSell

---
+++ Best_View_Dbase/bvt_prod/Views/XSell_FlightplanSalesForecast.sql
@@ -0,0 +1,16 @@
+﻿
+
+
+create view [bvt_prod].[XSell_FlightplanSalesForecast]
+as 
+select 
+	XSell_Flightplan_SalesRate_Daily_VW.idFlight_Plan_Records
+	, idkpi_type_FK as idkpi_types_FK
+	, idProduct_LU_TBL_FK
+	, Forecast_DayDate
+	, Sales_Rate_Daily*Volume as Sales_Forecast
+from bvt_prod.XSell_Flightplan_SalesRate_Daily_VW
+	inner join bvt_prod.XSell_Flightplan_Volume_Forecast_VW
+		on XSell_Flightplan_SalesRate_Daily_VW.idFlight_Plan_Records=XSell_Flightplan_Volume_Forecast_VW.idFlight_Plan_Records
+
+
