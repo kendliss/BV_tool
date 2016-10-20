950fa0b73e26a1b9f39e00559f1bb9f0c600f96c
kendliss <kliss@enterprisespectrum.com>
2016-10-20 15:14:32 -0500

    Updated table structues

---
+++ Best_View_Dbase/bvt_prod/Tables/Agency_LU_TBL.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE [bvt_prod].[Agency_LU_TBL](
+	[idAgency_LU_TBL] [int] IDENTITY(1,1) NOT NULL,
+	[Agency] [varchar](255) NULL,
+	[Agency_Description] [text] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idAgency_LU_TBL] ASC
+)
+
