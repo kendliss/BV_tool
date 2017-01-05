19ce38afee9ef7a3754ecef4b99cfb2f5743cd59
kendliss <kliss@enterprisespectrum.com>
2017-01-05 12:37:19 -0600

    Added Media Start Month
    Added media start month to account for MoveATT starting the first day of
    the Media Quarter

--- Best_View_Dbase/bvt_prod/Views/Movers_Best_View_Pivot_VW.sql
+++ Best_View_Dbase/bvt_prod/Views/Movers_Best_View_Pivot_VW.sql
@@ -6,6 +6,7 @@ CREATE VIEW [bvt_prod].[Movers_Best_View_Pivot_VW]
 	Select
 	[idFlight_Plan_Records_FK], [Campaign_Name], [InHome_Date], [Media_Year], [Media_Week], [Media_Month], [Touch_Name], [Program_Name], [Tactic], [Media], 
 	[Campaign_Type], [Audience], [Creative_Name], [Goal], [Offer], [Channel], [Scorecard_Group], [Scorecard_Program_Channel], CONVERT(VARCHAR(6),InHome_Date,112) AS Start_Month,
+	Media_Start_YYYYMM,
 sum(isnull([Call_CV], 0)) as [Call_CV], 
 sum(isnull([Online_CV], 0)) as [Online_CV], 
 sum(isnull([Online_sales_Access Line_CV], 0)) as [Online_sales_Access Line_CV], 
@@ -133,7 +134,7 @@ sum(isnull([Telesales_Access Line_AV], 0))+ sum(isnull([Telesales_DSL_AV], 0))+ 
 	FROM
 		(SELECT  
 	[idFlight_Plan_Records_FK], [Campaign_Name], [InHome_Date], [Media_Year], [Media_Week], [Media_Month], [Touch_Name], [Program_Name], [Tactic], [Media], 
-	[Campaign_Type], [Audience], [Creative_Name], [Goal], [Offer], [Channel], [Scorecard_Group], [Scorecard_Program_Channel], [Forecast], [Commitment], [Actual], [Best_View]
+	[Campaign_Type], [Audience], [Creative_Name], [Goal], [Offer], [Channel], [Scorecard_Group], [Scorecard_Program_Channel], Media_Start_YYYYMM, [Forecast], [Commitment], [Actual], [Best_View]
 	,Case when kpi_type in ('Response','Volume','Budget') then Product_Code+'_CV'
 		Else [KPI_Type]+'_'+[Product_Code]+'_CV' end as CV_metric 
 	,Case when kpi_type in ('Response','Volume','Budget') then Product_Code+'_FV'
@@ -143,7 +144,9 @@ sum(isnull([Telesales_Access Line_AV], 0))+ sum(isnull([Telesales_DSL_AV], 0))+ 
 		,Case when kpi_type in ('Response','Volume','Budget') then Product_Code+'_BV'
 		Else [KPI_Type]+'_'+[Product_Code]+'_BV' end as BV_metric 
 
-	FROM bvt_prod.Mover_Best_View_VW) as transform
+	FROM bvt_prod.Mover_Best_View_VW bv
+	JOIN (Select Distinct Date, MediaMonth, MediaMonth_Year, MediaMonth_YYYYMM as Media_Start_YYYYMM from DIM.Media_Calendar_Daily) cal
+	on InHome_Date = cal.Date) as transform
 
 	pivot 
 	(SUM(Commitment) for CV_METRIC IN ([Call_CV], 
@@ -268,4 +271,4 @@ sum(isnull([Telesales_Access Line_AV], 0))+ sum(isnull([Telesales_DSL_AV], 0))+ 
 [Volume_BV])) as P4
 
 group by [idFlight_Plan_Records_FK], [Campaign_Name], [InHome_Date], [Media_Year], [Media_Week], [Media_Month], [Touch_Name], [Program_Name], [Tactic], [Media], 
-	[Campaign_Type], [Audience], [Creative_Name], [Goal], [Offer], [Channel], [Scorecard_Group], [Scorecard_Program_Channel], CONVERT(VARCHAR(6),InHome_Date,112)
+	[Campaign_Type], [Audience], [Creative_Name], [Goal], [Offer], [Channel], [Scorecard_Group], [Scorecard_Program_Channel], CONVERT(VARCHAR(6),InHome_Date,112), Media_Start_YYYYMM
