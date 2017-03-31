6bfd895f301ddf9850da9b2a3f7202e0f280b29a
kendliss <kliss@enterprisespectrum.com>
2017-03-31 17:12:38 -0500

    Curve adj
    Changed curves to start at 0 so removed the calculation in forecast day
    date

--- Best_View_Dbase/bvt_prod/Views/Movers_Flightplan_SalesRate_Daily_VW.sql
+++ Best_View_Dbase/bvt_prod/Views/Movers_Flightplan_SalesRate_Daily_VW.sql
@@ -24,8 +24,8 @@ from
 	, idProduct_LU_TBL_FK
 	, Daily_Join.Day_of_Week
 	, Salesrate_Daily*week_percent as Sales_Rate_Daily
-	, DATEADD(week,c.Week_ID-1,InHome_Date) as Forecast_Week_Date
-	, DATEADD(day,Day_of_Week-1,DATEADD(week,c.Week_ID-1,InHome_Date)) as Forecast_DayDate
+	, DATEADD(week,c.Week_ID,InHome_Date) as Forecast_Week_Date
+	, DATEADD(day,Day_of_Week,DATEADD(week,c.Week_ID,InHome_Date)) as Forecast_DayDate
 	, ISO_week
 	, ISO_Week_Year
 	, MediaMonth
